c08f95421133e975b89babcd09ab8463677321ea
Miloš Lapiš <mlapis@mlc.cz>
2022-09-28 13:37:52 +0200

    wip(log): By interval

--- app/index.js
+++ app/index.js
@@ -23,5 +23,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log(`... listening on port ${port}`);
 	console.log('... application started');
-	i = setInterval(emitLog, 2000);
+	setInterval(emitLog, 2000);
 });
